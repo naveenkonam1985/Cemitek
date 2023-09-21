1f714520da1e3d19a6ec0554765401e8b5a4e8ee
Naveen Kumar Konam <52211514+naveenkonam1985@users.noreply.github.com>
2023-09-21 17:59:24 +0530

    Update streamlit_app.py
    added page config

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # File
 import streamlit as st
 
-menu_items = {"Get help":"", "About":"Cemitek is a sample webiste"}
+menu_items = {"Get help":"mailto:john@example.com", "About":"Cemitek is a sample webiste"}
 st.set_page_config(page_title='Cemitek', page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=menu_items)
 
 st.title("Cemitek")
