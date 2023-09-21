93993020cb02e072b2ca2b7729a0d92c0573d9ad
Naveen Kumar Konam <52211514+naveenkonam1985@users.noreply.github.com>
2023-09-21 17:58:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 # File
 import streamlit as st
 
+menu_items = {"Get help":"", "About":"Cemitek is a sample webiste"}
+st.set_page_config(page_title='Cemitek', page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=menu_items)
+
 st.title("Cemitek")
 st.write("Cement Plant Design")
+
+
