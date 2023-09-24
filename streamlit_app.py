ac54832814515c2d218b4af910da97c01fe06a58
Naveen Kumar Konam <52211514+naveenkonam1985@users.noreply.github.com>
2023-09-24 22:35:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,32 +7,6 @@ st.set_page_config(page_title='Cemitek', page_icon=None, layout="wide", initial_
 st.title("Cemitek")
 st.write("Cement Plant Design")
 
-def capacities():
-  st.session_state.calculate = True
-
-def clear():
-  st.session_state.calculate = False
-  st.session_state.tpd = ""
-
-if 'calculate' not in st.session_state:
-    st.session_state.calculate = False
-
-with st.sidebar:
-  st.write("Cemitek")
-  tpd_value = st.text_input(label="Enter plant capacity", key='tpd')
-  col1,col2 = st.columns([1,1])
-  
-  with col1:
-    st.button("Calculate",on_click=capacities, type='primary', use_container_width=True)
-      
-  with col2:
-    st.button("Clear", on_click=clear, use_container_width=True)
-  
-  if st.session_state.calculate:
-      if tpd_value is None or tpd_value == "":
-        st.write("Please Enter a value to proceed")
-        
-
 with st.container():
   col1,col2 = st.columns([1,1])
 
@@ -46,15 +20,7 @@ with st.container():
          st.write("Form Submitted")
        
   with col2:
-    plant_data = {}
-    if st.session_state.calculate:
-      if tpd_value:
-        st.write(f"Entered TPD value is: {tpd_value}")
-        tpd_value_corrected = int(tpd_value)
-    
-        plant_data['plant_capacity'] = (tpd_value_corrected * 330) / 1000000
-    
-        st.write(f"The plant capacity is: {plant_data['plant_capacity']} MTPA")
+    st.write("Resulting Data")
 
       
 
